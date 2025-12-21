2814db83328b338a80cc32d10b2bca6a798e7f59
omidhb <hajbagheri.o@gmail.com>
2025-12-21 15:50:08 +0330

    fix: add Vercel Blob storage domain to Next.js image configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     typedRoutes: false,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
